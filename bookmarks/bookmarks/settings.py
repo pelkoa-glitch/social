f0d59ab8205a6892719a02826dc86e0edda59d2b
pelkoa-glitch <74317301+pelkoa-glitch@users.noreply.github.com>
2024-04-08 20:55:35 +0300

    chapter 5, added HTTPS, Python Social Auth OAuth2.0 Google

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-_b*73sga(z7ky@%1wf3knirz81ht1f)-266@v0$s!2-d-z5(+w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -123,4 +125,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '720798197833-0qsb34g8o6g06lbfmg4cp3rodcjqomfr.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
