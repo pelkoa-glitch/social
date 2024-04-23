ce0d39436b412408d10fb22c08419cda5f11fc48
pelkoa-glitch <74317301+pelkoa-glitch@users.noreply.github.com>
2024-04-23 23:27:22 +0300

    added actions

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from actions.utils import create_action
 
 
 @login_required
@@ -21,6 +22,7 @@ def image_create(request):
             # Назначить текущего пользователя элементу
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(request,
                              'Image added successfully')
             # Перенаправить к представлению детальной
@@ -53,6 +55,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
