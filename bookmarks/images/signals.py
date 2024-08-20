8bb54050fd5b560026b8ae33cbcfdafb3c8b98f9
pelkoa-glitch <hvngmvnvibe@gmail.com>
2024-08-21 01:50:48 +0300

    fixed signals.py

--- bookmarks/images/signals.py
+++ bookmarks/images/signals.py
@@ -5,5 +5,5 @@ from .models import Image
 
 @receiver(m2m_changed, sender=Image.users_like.through)
 def user_like_changed(sender, instance, **kwargs):
-    instance.total_likes = instance.user_like.count()
+    instance.total_likes = instance.users_like.count()
     instance.save()
