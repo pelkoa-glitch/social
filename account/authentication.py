b70f9154b9060987c900a31609d3439c62879d3b
pelkoa-glitch <hvngmvnvibe@gmail.com>
2024-09-07 01:44:55 +0300

    refactoring

---
+++ account/authentication.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+
+class EmailAuthBackend:
+    """Аутентифицировать посредством адреса электронной почты."""
+    def authenticate(self, request, username=None, password=None):
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+        except User.MultipleObjectsReturned:
+            return None
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """Создать профиль пользователя для социальной аутентификации."""
+    Profile.objects.get_or_create(user=user)
