442512b65631a8a93e241386d510fc9defbf55bf
pelkoa-glitch <hvngmvnvibe@gmail.com>
2024-09-07 01:40:43 +0300

    refactoring

---
+++ images/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['created', 'slug', 'image', 'created']
+    list_filter = ['created']
